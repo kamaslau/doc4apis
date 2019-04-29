6f3206f3b2f492c8aef3d42aa206ab0f51920fa1
Kamas 'Iceberg' Lau <kamaslau@outlook.com>
2019-04-30 02:57:12 +0800

    更新class_name.vue模板

--- file_templates/view/vue/page/class_name.vue
+++ file_templates/view/vue/page/class_name.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="content">
     <div id="class-title" class="row">
-      <h2>文章</h2>
+      <h2>[[class_name_cn]]</h2>
   
       <nav id="nav-class">
         <ul>
           <nuxt-link class="btn btn-outline-secondary" :to="'/' + class_name" exact><i class="fal fa-list"></i> 所有</nuxt-link>
-          <nuxt-link class="btn btn-outline-secondary" :to="'/' + class_name + '/create'"><i class="fal fa-plus"></i> 添加</nuxt-link>
+          <nuxt-link class="btn btn-outline-secondary" :to="'/' + class_name + '/create'" exact><i class="fal fa-plus"></i> 添加</nuxt-link>
         </ul>
       </nav>
     </div>
@@ -19,8 +19,8 @@
 export default {
   data() {
     return {
-      class_name: 'article',
-      id_name: 'article_id'
+      class_name: '[[class_name]]',
+      id_name: '[[id_name]]'
     }
   }
 
