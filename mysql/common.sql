c916b7dcfa0ccd643915ad58d9807374736f2433
Kamas 'Iceberg' Lau <kamaslau@outlook.com>
2021-03-11 06:28:03 +0800

    Fixed 常用SQL语句的注释错误

--- mysql/common.sql
+++ mysql/common.sql
@@ -7,7 +7,7 @@
  */
 ALTER TABLE `table_name`
     ADD COLUMN `time_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' AFTER `name_of_last_column`,
-    ADD COLUMN `time_delete` datetime NULL COMMENT '删除时间；UNIX时间戳' AFTER `time_create`,
+    ADD COLUMN `time_delete` datetime NULL COMMENT '删除时间' AFTER `time_create`,
     ADD COLUMN `time_edit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后操作时间' AFTER `time_delete`,
     ADD COLUMN `creator_id` int(11) UNSIGNED NOT NULL COMMENT '创建者用户ID' AFTER `time_edit`,
     ADD COLUMN `operator_id` int(11) UNSIGNED NULL COMMENT '最后操作者用户ID' AFTER `creator_id`;
